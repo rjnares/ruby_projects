c84a234bee5ec4c2400b1ab7019c060bda901a4b
Roberto Nares <roberto.nares@gmail.com>
2024-07-09 13:42:28 -0700

    Implement caesar cipher method

---
+++ caesar_cipher.rb
@@ -0,0 +1,14 @@
+def caesar_cipher(string, shift_factor)
+  shifted_codepoints = string.codepoints.map do |codepoint|
+    if codepoint.between?(65, 90) || codepoint.between?(97, 122)
+      base = codepoint < 97 ? 65 : 97
+      ((codepoint - base + shift_factor) % 26) + base
+    else
+      codepoint
+    end
+  end
+  
+  p shifted_codepoints.map { |codepoint| codepoint.chr }.join
+end
+
+caesar_cipher("What a string!", 5)
